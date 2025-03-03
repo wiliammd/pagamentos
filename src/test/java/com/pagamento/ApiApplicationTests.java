b518a10cf4458c0bbe3bb531028c5d1a257f29f6
Wiliam M. D'Epiro <Wiliam.depiro@hotmail.com>
2025-03-02 22:35:34 -0300

    remove start context

--- src/test/java/com/pagamento/ApiApplicationTests.java
+++ src/test/java/com/pagamento/ApiApplicationTests.java
@@ -1,25 +1,10 @@
 package com.pagamento;
 
-import com.pagamento.config.TestSqsConfig;
-import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-@SpringBootTest(classes = TestSqsConfig.class)
 class ApiApplicationTests {
 
 
-//	@Mock
-//	private SqsAsyncClient sqsAsyncClient;
-//
-//	@Mock
-//	private SqsTemplate sqsTemplate;
-//
-//	@InjectMocks
-//	private ApiApplication apiApplication;
 	@Test
 	void contextLoads() {
 	}
